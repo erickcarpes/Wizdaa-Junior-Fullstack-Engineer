7097b76f279c6539cf1ce951d7d47b64f44fbfb4
Erick Carpes <erickcarpesmmc@gmail.com>
2026-04-23 23:58:42 -0300

    feat: wire application bootstrap

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '@/infrastructure/prisma/prisma.module';
+import { BalancesModule } from '@/modules/balances/balances.module';
+import { HcmIntegrationModule } from '@/modules/hcm-integration/hcm-integration.module';
+import { MockHcmModule } from '@/modules/mock-hcm/mock-hcm.module';
+import { ReconciliationModule } from '@/modules/reconciliation/reconciliation.module';
+import { TimeOffRequestsModule } from '@/modules/time-off-requests/time-off-requests.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    BalancesModule,
+    TimeOffRequestsModule,
+    HcmIntegrationModule,
+    MockHcmModule,
+    ReconciliationModule,
+  ],
+})
+export class AppModule {}
